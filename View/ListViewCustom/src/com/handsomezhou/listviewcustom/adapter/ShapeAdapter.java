978a9e2a71575a48eb1ceec92864200317cf780e
handsomezhou <quanjunzhou@gmail.com>
2014-08-29 23:57:25 +0800

    Improve Listview  efficiency

--- View/ListViewCustom/src/com/handsomezhou/listviewcustom/adapter/ShapeAdapter.java
+++ View/ListViewCustom/src/com/handsomezhou/listviewcustom/adapter/ShapeAdapter.java
@@ -10,7 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.handsomezhou.listviewcustom.R;
+import com.handsomezhou.listviewcustom.activity.R;
 import com.handsomezhou.listviewcustom.model.Shape;
 
 public class ShapeAdapter extends ArrayAdapter<Shape> {
@@ -25,16 +25,37 @@ public class ShapeAdapter extends ArrayAdapter<Shape> {
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
 		Shape shape = getItem(position);
-		View view = LayoutInflater.from(getContext()).inflate(
-				mTextViewResourceId, null);
-		ImageView shapeImage = (ImageView) view
-				.findViewById(R.id.shape_image_image_view);
-		TextView shapeName = (TextView) view
-				.findViewById(R.id.shape_name_text_view);
-		shapeImage.setImageResource(shape.getImageId());
-		shapeName.setText(shape.getName());
+		View view;
+		ViewHolder viewHolder;
+		if (null == convertView) {
+			view = LayoutInflater.from(getContext()).inflate(
+					mTextViewResourceId, null);
+			viewHolder = new ViewHolder();
+			viewHolder.shapeImage = (ImageView) view
+					.findViewById(R.id.shape_image_image_view);
+			viewHolder.shapeName = (TextView) view
+					.findViewById(R.id.shape_name_text_view);
+			view.setTag(viewHolder);
+		} else {
+			view = convertView;
+			viewHolder = (ViewHolder) view.getTag();
+		}
+
+		// ImageView shapeImage = (ImageView) view
+		// .findViewById(R.id.shape_image_image_view);
+		// TextView shapeName = (TextView) view
+		// .findViewById(R.id.shape_name_text_view);
+		// shapeImage.setImageResource(shape.getImageId());
+		// shapeName.setText(shape.getName());
+		viewHolder.shapeImage.setImageResource(shape.getImageId());
+		viewHolder.shapeName.setText(shape.getName());
 
 		return view;
+	}
+
+	class ViewHolder {
+		ImageView shapeImage;
+		TextView shapeName;
 	}
 
 }
