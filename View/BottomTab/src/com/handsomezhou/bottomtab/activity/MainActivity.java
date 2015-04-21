d3cdab4a4ee8c843f2572380d2a8ba9fbc8a2b40
handsomezhou <quanjunzhou@gmail.com>
2015-04-22 07:02:20 +0800

    add bottom tab

---
+++ View/BottomTab/src/com/handsomezhou/bottomtab/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.handsomezhou.bottomtab.activity;
+
+import com.handsomezhou.bottomtab.R;
+import com.handsomezhou.bottomtab.R.layout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+
+}
